51d25324368b2702418d1d9e7010e7969546f265
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-10-13 20:39:42 +0300

    all fix

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,55 +10,35 @@ const getContacts = async (req, res, next) => {
   }
 };
 const getContactsById = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-
-    const contact = await contacts.getContactById(contactId);
-
-    if (!contact) {
-      throw RequesError(404, "Not found");
-    }
-
-    res.status(200).json(contact);
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId);
+  if (!contact) {
+    throw RequesError(404, "Not found");
   }
+  res.status(200).json(contact);
 };
 
 const addContacts = async (req, res, next) => {
-  try {
-    const addContacts = await contacts.addContact(req.body);
-    res.status(201).json(addContacts);
-  } catch (error) {
-    next(error);
-  }
+  const addContacts = await contacts.addContact(req.body);
+  res.status(201).json(addContacts);
 };
 
 const apdateContacts = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const apdateContact = await contacts.updateContact(contactId, req.body);
-    if (!apdateContact) {
-      throw RequesError(404, "Not found");
-    }
-    res.status(200).json(apdateContact);
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const apdateContact = await contacts.updateContact(contactId, req.body);
+  if (!apdateContact) {
+    throw RequesError(404, "Not found");
   }
+  res.status(200).json(apdateContact);
 };
 
 const removeContacts = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const deleteContacts = await contacts.removeContact(contactId);
-
-    if (!deleteContacts) {
-      throw RequesError(404, "Not found");
-    }
-    res.status(200).json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
+  const { contactId } = req.params;
+  const deleteContacts = await contacts.removeContact(contactId);
+  if (!deleteContacts) {
+    throw RequesError(404, "Not found");
   }
+  res.status(200).json({ message: "contact deleted" });
 };
 
 module.exports = {
