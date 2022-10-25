fddfa8837059d65c563ca1c538947c79295d2cc7
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-10-25 21:11:27 +0300

    done hw-4

--- controllers/logout.js
+++ controllers/logout.js
@@ -3,7 +3,7 @@ const { User } = require("../models/users");
 const logoute = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  await res.status(204);
+  res.status(204);
 };
 
 module.exports = logoute;
