ef5e8952d6754c23b1d567c1379f99bdb5c8141d
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-11-02 14:33:13 +0300

    hw05-avatars

--- routes/api/filesRouter.js
+++ routes/api/filesRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const multer = require("multer");
 const path = require("path");
-// const fs = require("fs/promises");
+const fs = require("fs/promises");
 
 const contacts = [];
 
